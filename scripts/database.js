8cee9e8d5fb30f96eda710ce5fac5574d70de03e
Justin Sweeten <jtsweeten@gmail.com>
2022-09-15 10:19:09 -0500

    added program details in database.js

--- scripts/database.js
+++ scripts/database.js
@@ -14,18 +14,68 @@ const database = {
     }
 ],
 
-  programs: ["Matlock",
-    "Murder, She Wrote",
-    "Columbo",
-    "Magnum P.i.",
-    "Starsky & Hutch",
-    "Miami Vice",
-    "Hawaii Five-0",
-    "Dragnet",
-    "MacGyver",
-    "True Blue",
-    "Police Squad!",
-    "Knight Rider"]
+  programs: [
+    {
+      id: 1,
+      name: "Matlock",
+      time: "Mondays @ 5:30pm"
+    },
+    {
+      id: 2,
+      name: "Murder, She Wrote",
+      time: "Saturdays @ 2:00pm"
+    },
+    {
+      id: 3,
+      name: "Columbo",
+      time: "Wednesdays @ 5:30pm"
+    },
+    {
+      id: 4,
+      name: "Magnum P.i.",
+      time: "Thursdays @ 6:30pm"
+    },
+    {
+      id: 5,
+      name: "Starsky & Hutch",
+      time: "Fridays @ 5:30pm"
+    },
+    {
+      id: 6,
+      name: "Miami Vice",
+      time: "Tuesdays @ 7:00pm"
+    },
+    {
+      id: 7,
+      name: "Hawaii Five-0",
+      time: "Sundays @ 6:00pm"
+    },
+    {
+      id: 8,
+      name: "Dragnet",
+      time: "Mondays @ 6:30pm"
+    },
+    {
+      id: 9,
+      name: "MacGyver",
+      time: "Thursdays @ 7:30pm"
+    },
+    {
+      id: 10,
+      name: "True Blue",
+      time: "Wednesdays @ 6:30pm"
+    },
+    {
+      id: 11,
+      name: "Police Squad!",
+      time: "Tuesdays @ 6:00pm"
+    },
+    {
+      id: 12,
+      name: "Knight Rider",
+      time: "Sundays @ 7:00pm"
+    }
+  ]
 }
 
 export const getShows = () => {
